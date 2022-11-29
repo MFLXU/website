b1538c4517de2ff7a9040fac17453e7064f497e7
Lou+^*! <90705868+MFLXU@users.noreply.github.com>
2022-11-30 00:39:58 +0100

    wo;irtejlw

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Navbar from "./components/Navbar";
 import { useTranslation } from "react-i18next";
-
 function App() {
-  return <div></div>;
+  const { t, i18n } = useTranslation();
+  return (
+    <div className="">
+      <Navbar />
+      <p>{t("title")}</p>
+      <p>{t("description.part1")}</p>
+      <p>{t("description.part2")}</p>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,45 @@
+import dz from "../utils/algeria.png";
+import uk from "../utils/uk.png";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { MoonIcon } from "@heroicons/react/20/solid";
 const Navbar = () => {
-  return <div>
-    <div className="container"></div>
-  </div>
-}
-export default Navbar
+  const { t, i18n } = useTranslation();
+  const [lang, setLang] = useState("uk");
+  return (
+    <div className="py-4">
+      <div className="container flex items-center justify-between">
+        <a className="font-extrabold text-3xl" href="/">
+          Debug Club Biskra
+        </a>
+        <div className="flex items-center justify-center gap-2">
+          <div className="outline-0 flex items-center justify-center p-2 border rounded-md">
+            <MoonIcon className="w-4" />
+          </div>
+          {lang == "uk" ? (
+            <button
+              onClick={() => {
+                i18n.changeLanguage("ar");
+                setLang("ar");
+              }}
+              className="outline-0 flex items-center justify-center p-2 border rounded-md"
+            >
+              <img className="w-8" src={dz} alt="" />
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                i18n.changeLanguage("en");
+                setLang("uk");
+              }}
+              className="outline-0 flex items-center justify-center p-2 border rounded-md"
+            >
+              <img className="w-8" src={uk} alt="" />
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
