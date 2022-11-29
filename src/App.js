10ca7d6a4364328a03700d2b0f66ff2e86f65860
Lou+^*! <90705868+MFLXU@users.noreply.github.com>
2022-11-29 23:28:31 +0100

    ////

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-
+import { useTranslation } from 'react-i18next';
 function App() {
+    const { t, i18n } = useTranslation();
   return (
-   <div></div>
+   <div>
+       {t('Welcome to React')}
+   </div>
   );
 }
 
