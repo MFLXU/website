537dc488bdf59f995c25420a7b844f80395141f3
Lou+^*! <90705868+MFLXU@users.noreply.github.com>
2022-11-30 00:20:41 +0100

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+
 function App() {
-    const { t, i18n } = useTranslation();
-  return (
-   <div>
-       {t('Welcome to React')}
-   </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/i18n.js
+++ src/i18n.js
@@ -1,27 +1,38 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import translationEN from "./locate/en.json";
+import translationAR from "./locate/ar.json";
 
-
-
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  ar: {
+    translation: translationAR,
+  },
+};
 i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    debug: true,
 
-    .use(Backend)
-
-    .use(LanguageDetector)
-
-    .use(initReactI18next)
-
-    .init({
-        fallbackLng: 'en',
-        debug: true,
-
-        interpolation: {
-            escapeValue: false,
-        }
-    });
-
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: {
+        translation: translationEN,
+      },
+      ar: {
+        translation: translationAR,
+      },
+    },
+  });
 
 export default i18n;
