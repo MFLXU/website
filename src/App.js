6814d856552e3fde00787ba670ea96a8a52e5b79
Lou+^*! <90705868+MFLXU@users.noreply.github.com>
2022-11-30 19:08:04 +0100

    hgcjcchk

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import Navbar from "./components/Navbar";
 import { useTranslation } from "react-i18next";
 import { createContext } from "react";
 import { useState } from "react";
+import Hero from "./components/Hero";
 export const LanguageContext = createContext("uk");
 function App() {
   const [lang, setLang] = useState("uk");
   const { t, i18n } = useTranslation();
   return (
-    <div className="">
+    <div className={lang == "uk" ? "text-left" : "text-right"}>
       <LanguageContext.Provider value={{ lang, setLang }}>
         <Navbar />
       </LanguageContext.Provider>
-      <p>{t("title")}</p>
-      <p>{t("description.part1")}</p>
-      <p>{t("description.part2")}</p>
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,56 @@
+import heroImg from "../utils/hero.svg";
+import { useTranslation } from "react-i18next";
+import { useContext } from "react";
+import { LanguageContext } from "../App";
+const Hero = () => {
+  const { lang } = useContext(LanguageContext);
+  const { t } = useTranslation();
+  return (
+    <section className="py-40">
+      {lang == "uk" ? (
+        <div className="container grid md:grid-cols-2 gap-8">
+          <div className="flex justify-start flex-col">
+            <h2 className="text-2xl font-medium">{t("hero.intro")}</h2>
+            <h1 className="text-6xl font-extrabold text-[#ff2c62] mb-4">
+              {t("hero.clubname")}
+            </h1>
+            <p className="text-xl">{t("hero.desc")}</p>
+            <div className="grid md:grid-cols-2 gap-4 mt-auto">
+              <button className="p-4 text-xl font-medium rounded-md border border-dbc text-dbc">
+                {t("hero.buttons.btn1")}
+              </button>
+              <button className="p-4 text-xl font-medium rounded-md bg-dbc text-white">
+                {t("hero.buttons.btn2")}
+              </button>
+            </div>
+          </div>
+          <div>
+            <img src={heroImg} alt="" />
+          </div>
+        </div>
+      ) : (
+        <div className="container grid md:grid-cols-2 gap-8">
+          <div>
+            <img src={heroImg} alt="" />
+          </div>
+          <div className="flex justify-start flex-col">
+            <h2 className="text-2xl font-medium">{t("hero.intro")}</h2>
+            <h1 className="text-6xl font-extrabold text-[#ff2c62] mb-4">
+              {t("hero.clubname")}
+            </h1>
+            <p className="text-xl">{t("hero.desc")}</p>
+            <div className="grid md:grid-cols-2 gap-4 mt-auto">
+              <button className="p-4 text-xl font-medium rounded-md bg-dbc text-white">
+                {t("hero.buttons.btn2")}
+              </button>
+              <button className="p-4 text-xl font-medium rounded-md border border-dbc text-dbc">
+                {t("hero.buttons.btn1")}
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+export default Hero;
